7269eea6ad74cecf7dd86381e80263ef68d1bbf3
fazal790207 <zal72@hotmail.com>
2017-04-13 15:44:40 +0800

    Update login_view.php

--- application/views/login/login_view.php
+++ application/views/login/login_view.php
@@ -5,7 +5,10 @@
 </style>
 <div class="row wrapper align-center">
   <div class="medium-4 medium-centered small-4 small-centered large-4 large-centered columns ">
-    // Login page
+
+
+
+//wkaka
     <form action="<?php echo base_url();?>login/verify" method="POST" data-abide novalidate>
       <div class="row column box-login">
         <div class="row column log-in-form">
@@ -17,14 +20,14 @@
               Please fill in your username , it's required.
             </span>
           </label>
-          
+          /kekekekeke
           <label>Password
             <input type="password" name="passwordLogin" placeholder="Password" required>
             <span class="form-error">
               Please fill in your password , it's required.
             </span>
           </label>
-          
+          //kekekekkejhhf
           <p><input type="submit" class="button expanded button_hitam" value="Log In" name="btnSubmit"></p>
           <p class="text-center"><a href="#" class="forgot_password">Forgot your password?</a></p>   
         </div>
